966109a37c5c1c96b2657f3c637e340970f830c2
Peca Lee <studiopeca.lee@gmail.com>
2021-07-19 16:53:06 +0900

    req, res, middleware

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # WeTube Clone coding
 
 - node.js
+- babel
+- nodemon
 - express
+  - import express
+  - app.get()
+  - request
+  - response
+  - middleware
+  - app.use()
--- src/server.js
+++ src/server.js
@@ -1,16 +1,25 @@
 "use strict";
 
-import express from "express"
+import express from "express";
 
-const PORT = 4000
-const app = express()
+const PORT = 4000;
+const app = express();
 
-const handleHome = (req, res) => {
-    console.log("Welcom my home.")
-    return res.send("Welcome!")
-}
+const pathLogger = (req, res, next) => {
+  console.log("PATH : ", req.path);
+  next();
+};
 
-app.get("/", handleHome)
+const methodLogger = (req, res, next) => {
+  console.log("Method : ", req.method);
+  next();
+};
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`)
-app.listen(PORT, handleListening)
+const home = (req, res) => res.send("<h1>Hello Home</h1>");
+
+app.use(methodLogger, pathLogger);
+app.get("/", home);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT} 🚀`);
+app.listen(PORT, handleListening);
